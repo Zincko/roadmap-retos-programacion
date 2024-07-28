0268690a33f2c20e128cf5315893a6bba370ba3f
Zincko <setzel05@gmail.com>
2024-07-28 02:38:44 +0200

    Create Zincko.js

---
+++ Roadmap/Zincko.js
@@ -0,0 +1,36 @@
+// URL del sitio web oficial de JavaScript
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Representación de diferentes sintaxis de comentarios
+
+// Esto es un comentario de una sola línea
+
+/*
+  Esto es un comentario
+  de varias líneas
+*/
+
+// Crear una variable y una constante
+let variableEjemplo = "Soy una variable"; // Uso de let para declarar una variable
+const CONSTANTE_EJEMPLO = "Soy una constante"; // Uso de const para declarar una constante
+
+// Crear variables representando todos los tipos de datos primitivos
+let cadenaTexto = "Hola, soy una cadena de texto"; // String
+let numeroEntero = 42; // Number
+let numeroDecimal = 3.14; // Number
+let valorBooleano = true; // Boolean
+let valorNulo = null; // Null
+let valorIndefinido; // Undefined (por defecto es undefined)
+
+// Imprimir por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+console.log("¡Hola, JavaScript!");
+
+// Mostrar todas las variables por consola
+console.log(variableEjemplo);
+console.log(CONSTANTE_EJEMPLO);
+console.log(cadenaTexto);
+console.log(numeroEntero);
+console.log(numeroDecimal);
+console.log(valorBooleano);
+console.log(valorNulo);
+console.log(valorIndefinido);
