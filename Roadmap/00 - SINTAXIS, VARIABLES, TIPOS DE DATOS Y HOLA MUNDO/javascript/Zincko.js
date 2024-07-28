952aa7bb28ed10b99ddea85a8d475983015fe659
Zincko <setzel05@gmail.com>
2024-07-28 02:05:33 +0200

    Create Zincko.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Zincko.js
@@ -0,0 +1,36 @@
+// IRL del sitio web oficial de JavaScript
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Representación de diferentes sitaxis de comentarios
+
+// Este es un comentario en una sola linea
+
+/* 
+este es un comentario
+en varias lineas 
+*/
+
+// Crear una variable y una constante
+let variableEjemplo = "Soy una variable"; // Uso de let para declarar una variable de bloque
+const CONSTANTE_EJEMPLO ="Soy una constante"; // Uso de const para declarar una constante
+
+// Variables represenatando todo tipo de datos primitivos
+let candenaTexto = "Hola soy una cadena de texto"; // String
+let numeroEntero = 42; // Number
+let numeroDecimal = 3.21; // Number
+let valorBooleano = true; // Boolean
+let valorNulo = null; // Null
+let valorIndefinido; // Undefined (Por defecto es undefined)
+
+// Imprimir por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+console.log("Hola, JavaScript");
+
+// Mostrar todas las variables por consola
+console.log(variableEjemplo);
+console.log(CONSTANTE_EJEMPLO);
+console.log(candenaTexto);
+console.log(numeroEntero);
+console.log(numeroDecimal);
+console.log(valorBooleano);
+console.log(valorNulo);
+console.log(valorIndefinido);
